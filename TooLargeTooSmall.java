05e68c22444b48a94680953bcd0b8fc87d3a36a8
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-06-26 00:22:32 -0400

    if else added to pass tests

--- TooLargeTooSmall.java
+++ TooLargeTooSmall.java
@@ -7,18 +7,28 @@
  */
 public class TooLargeTooSmall {
     private Integer chosenNum;
+   
 
     /**
      * Constructor for objects of class TooLargeTooSmall
      */
     public TooLargeTooSmall(Integer seed) {
         // initialise instance variables
-        chosenNum = seed;
+      chosenNum = seed;
+      
     }
     
     public Integer guess(Integer g) {
         
-        return chosenNum;
+        if (g > chosenNum){
+            return 1;    
+        } else if (g < chosenNum){
+            return -1;
+        } else {
+            return 0;
+        }
+        
+ 
 
     }
 }
